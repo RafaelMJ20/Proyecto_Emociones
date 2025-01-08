08882cb24c30272e618da52b8da7732c16165ea8
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2025-01-08 08:28:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,10 @@ import base64
 import json
 import os
 from tensorflow.keras.models import model_from_json
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseUpload
 import mediapipe as mp
 
 app = Flask(__name__)
 CORS(app)
-
-# ID de la carpeta donde deseas subir la imagen
-FOLDER_ID = '1Z5oK0YBGg8HFsbpmsUWFwKqtczKXZxPX'
 
 # Diccionario de emociones
 label_to_text = {0: 'Ira', 1: 'Odio', 2: 'Tristeza', 3: 'Felicidad', 4: 'Sorpresa'}
@@ -66,25 +60,17 @@ def predict():
     emotion = label_to_text[emotion_index]
     confidence = predictions[0][emotion_index]
 
-    # Detectar puntos faciales y dibujarlos
-    img_data = process_and_save_image(image_bytes, image_file.filename)
+    # Detectar puntos faciales y obtener la imagen procesada
+    img_data = process_image(image_bytes, image_file.filename)
 
     return jsonify({
         'emotion': emotion,
         'confidence': float(confidence),
-        'image_base64': img_data['image_base64'],
-        'drive_id': img_data['drive_id']
+        'image_base64': img_data['image_base64']
     })
 
-def obtener_servicio_drive():
-    SCOPES = ['https://www.googleapis.com/auth/drive.file']
-    creds_info = json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON'))
-    creds = service_account.Credentials.from_service_account_info(creds_info, scopes=SCOPES)
-    service = build('drive', 'v3', credentials=creds)
-    return service
-
-def process_and_save_image(image_bytes, filename):
-    """Convierte la imagen, detecta puntos faciales y la sube a Google Drive."""
+def process_image(image_bytes, filename):
+    """Convierte la imagen, detecta puntos faciales y la procesa."""
     # Convertir la imagen a formato PIL para posibles manipulaciones
     image = Image.open(io.BytesIO(image_bytes))
     img_np = np.array(image)
@@ -110,19 +96,8 @@ def process_and_save_image(image_bytes, filename):
     image.save(buffered, format="PNG")
     img_data = buffered.getvalue()
 
-    # Subir a Google Drive
-    service = obtener_servicio_drive()
-    archivo_drive = MediaIoBaseUpload(io.BytesIO(img_data), mimetype='image/png')
-    archivo_metadata = {
-        'name': f'{filename}',
-        'mimeType': 'image/png',
-        'parents': [FOLDER_ID]
-    }
-    archivo_subido = service.files().create(body=archivo_metadata, media_body=archivo_drive).execute()
-
     return {
-        'image_base64': base64.b64encode(img_data).decode('utf-8'),
-        'drive_id': archivo_subido.get('id')
+        'image_base64': base64.b64encode(img_data).decode('utf-8')
     }
 
 if __name__ == '__main__':
